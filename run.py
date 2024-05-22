f87067b670865bd85ab15561e127bfd2a1b3d0ec
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-05-22 14:07:52 +0000

    add print messages with game info to main-function

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-
+class Board:
+    """
+    Main board. Creates both players battlefield according to given size, placing ships on the boards 
+    and handling players shots. __init__ function created with the help of Project 3 Portfolio Scope.
+    """
+    def __init__(self, size, num_ships, user_name):
+        self.size = size
+        self.num_ships = num_ships
+        self.user_name = user_name
+        self.board = [['~' for _ in range(size)] for _ in range(size)]
+        self.guesses = []
+        self.ships = []
 
 def get_username():
     """
@@ -17,6 +28,12 @@ def get_username():
             print("Oops, not a valid username. Please enter your name again.")  
 
 def main():
-    get_username()  
+    print("========================================\n")
+    print("Welcome to the great Battle of the ships!")
+    print("Board size: 6x6. Numb of ships: 5.")
+    print("Top left corner is row 0, col 0\n")
+    print("========================================\n")
+    user_name = get_username()  
+    print("========================================\n")
 
 main()               
