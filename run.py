db5ff3969ee9af4071379eb4392d7bc7b9edf298
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-05-22 14:03:59 +0000

    create main function to store and  call other functions

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
-class Board:
+
+
+def get_username():
     """
-    Main board. Creates both players battlefield according to given size, placing ships on the boards 
-    and handling players shots. __init__ function created with the help of Project 3 Portfolio Scope.
+    Function for getting players name and thus creating a username.
+    Contains validation for correct input.
     """
-    def __init__(self, size, num_ships, user_name):
-        self.size = size
-        self.num_ships = num_ships
-        self.user_name = user_name
-        self.board = [['~' for _ in range(size)] for _ in range(size)]
-        self.guesses = []
-        self.ships = []
+    print("Let's start by adding a username")
+    print("Username must be one word, letters only.")
+    while True:
+        user_name = input("Please enter your name here: \n")
+        print()
+        if user_name.isalpha():
+            print(f"Hello and welcome {user_name}, let the battle begin!\n")
+            return user_name
+        else:
+            print("Oops, not a valid username. Please enter your name again.")  
+
+def main():
+    get_username()  
+
+main()               
